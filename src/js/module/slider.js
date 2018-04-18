b76a5bfc6fce815dbfd96e9a4a5f8c78735191eb
Dmitry Britan <dmitry.britan@gmail.com>
2018-04-18 22:53:44 +0300

    Добавил слайдеры для доп товаров на страницы Корзина

--- src/js/module/slider.js
+++ src/js/module/slider.js
@@ -11,3 +11,27 @@ if ($('.js-slider').length) {
 		slidesToScroll: 1,
 	});
 }
+//
+// Products Slider on Cart Page
+// =================================================================
+if ($('.js-products-slider').length) {
+	$('.js-products-slider').slick({
+		arrows: false,
+		dots: true,
+		adaptiveHeight: true,
+		infinite: true,
+		speed: 400,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 4000,
+				settings: 'unslick',
+			},
+			{
+				breakpoint: 640,
+				settings: 'slick',
+			},
+		],
+	});
+}
